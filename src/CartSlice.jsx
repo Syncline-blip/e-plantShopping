90fec1a8b01c0f6b9b36474d8e5d720951591c6d
JL <jadlumagbas@gmail.com>
2025-01-10 16:27:33 +0800

    Task 2 Done

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,22 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(items => items.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({itme, quantity: 1, cost, image});
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const itemToIncrease = state.items.find(items => items.name === name);
+      if (itemToIncrease) {
+        itemToIncrease.quantity++;
+      }
     },
   },
 });
